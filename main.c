a5ac2ce2db9f72515c78bd76194bf31f1cf59c22
John Sharp <johncolinsharp@gmail.com>
2018-03-09 22:51:43 +0000

    updated for change in engine

--- main.c
+++ main.c
@@ -2,38 +2,77 @@
 
 #include <stdio.h>
 
+enum
+{
+    DECAL_DOG,
+    NUM_DECALS
+};
+
 typedef struct dogActor
 {
     actor a;
-    sprite * s;
+    sprite s;
 } dogActor;
+
+typedef struct herdingSheepsEngine
+{
+    engine * engine;
+
+    decal decals[NUM_DECALS];
+
+    dogActor dog;
+} herdingSheepsEngine;
 
 void dogRenderHandler(actor *a)
 {
     dogActor * d = a->owner;
-
-    engineSpriteRender(a->eng, d->s);
+    engineSpriteRender(a->eng, &d->s);
 }
 
 void dogLogicHandler(actor *a)
 {
 }
 
+void dogUpdateSprite(dogActor * dog)
+{
+    juint w = dog->s.d->rect.tr[0] - dog->s.d->rect.bl[0];
+    juint h = dog->s.d->rect.tr[1] - dog->s.d->rect.bl[1];
+    dog->s.rect.bl[0] = 100 - w/2;
+    dog->s.rect.tr[0] = 100 + w/2;
+
+    dog->s.rect.bl[1] = 100 - h/2;
+    dog->s.rect.tr[1] = 100 + h/2;
+}
+
+void initDog(dogActor * dog, herdingSheepsEngine * eng)
+{
+    dog->a.owner = dog;
+    dog->a.renderHandler = dogRenderHandler;
+    dog->a.logicHandler = dogLogicHandler;
+    engineActorReg(eng->engine, &dog->a);
+    dog->s.d = &eng->decals[DECAL_DOG];
+    dogUpdateSprite(dog);
+}
+
+herdingSheepsEngine * initHerdingSheepsEngine(herdingSheepsEngine * eng)
+{
+    eng->engine = createEngine(800, 600, eng);
+
+    juint dogTexture = engineLoadTexture(eng->engine, "assets/imgs/dog/E_still.png");
+    decalInit(&eng->decals[DECAL_DOG], eng->engine,
+            dogTexture, engineGetTextureRect(eng->engine, dogTexture));
+
+    initDog(&eng->dog, eng);
+
+    return eng;
+}
+
+
+
 int main()
 {
-    engine * e = createEngine(800, 600);
+    herdingSheepsEngine eng;
+    initHerdingSheepsEngine(&eng);
 
-    juint dogTexture = engineLoadTexture(e, "assets/imgs/dog/E_still.png");
-    decal dogDecal;
-    decalInit(&dogDecal, e, dogTexture, engineGetTextureRect(e, dogTexture));
-
-    sprite dogSprite = {d : &dogDecal, rect: engineGetTextureRect(e, dogTexture)};
-
-    dogActor dogActor = {s : &dogSprite, a : {renderHandler : dogRenderHandler,
-        logicHandler : dogLogicHandler }};
-    dogActor.a.owner = &dogActor;
-
-    engineActorReg(e, &dogActor.a);
-
-    engineStart(e);
+    engineStart(eng.engine);
 }
